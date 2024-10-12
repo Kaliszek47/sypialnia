9fe5923b79962236703b7951cf6cad18527948dd
Kaliszek47 <nkaliszek@gmail.com>
2024-10-12 17:15:25 +0200

    added blah blah

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HEJKA") ;
+
+        int number =5;
+
+        System.out.println(number);
+
+        number += 10;
+
+        System.out.println(number);
+
+        int num1=6 , num2=7;
+        int sum= num1 + num2;
+
+        System.out.println(sum);
+
+        float num3=6, num4=7;
+        float dzielenie;
+
+        dzielenie= num4/num3;
+
+        System.out.println(dzielenie);
+
+        boolean bool1=true;
+        boolean bool2=false;
+
+        System.out.println(bool1 && bool2);
+        System.out.println(bool1||bool2);
+        System.out.println(!(bool1||bool2));
+
     }
 }
