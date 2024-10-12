6259fa7576b61933e13e70cab110e0c66c3531a9
Kaliszek47 <nkaliszek@gmail.com>
2024-10-12 18:13:11 +0200

    scanner

--- Main.java
+++ Main.java
@@ -29,5 +29,47 @@ public class Main {
         System.out.println(bool1||bool2);
         System.out.println(!(bool1||bool2));
 
+        int n1= 6, n2=10;
+
+            System.out.println(n1 + " " + n2);
+
+        if(n1>n2){
+            System.out.println("liczba " + n1 + " jest wieksza od liczby" + n2);
+        }
+        else if(n1==n2){
+            System.out.println("liczby są sobie równe");
+        }
+        else {
+            System.out.println("liczba " + n1 + " /jest mniejsza od liczby " + n2);
+        }
+
+            int x=0;
+
+        for(int i=0; i<10; i++){
+            x+=1 ;
+            System.out.println(x);
+        }
+
+        System.out.println("x+1");
+
+        for(int i=0; i<10; i++){
+            System.out.println(i+1);
+        }
+
+        System.out.println("i+1");
+
+        int i=0;
+        while (i<20) {
+            System.out.println(i + " ");
+            i++;
+        }
+
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
     }
+
 }
